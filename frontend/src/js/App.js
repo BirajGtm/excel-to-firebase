d15bd753635e5d89040d96d5c3c50ca720961350
Suzal3579 <pe.messh@gmail.com>
2018-03-23 23:30:10 +0530

    View all button made and fully working, Fetching data from firebase working ...

--- frontend/src/js/App.js
+++ frontend/src/js/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import UploadForm from './UploadForm';
+import ListAll from './ListAll';
 
 const App = () => (
     <div className="App">
         <h1>File Upload</h1>
         <UploadForm />
+        <ListAll />
     </div>
 );
 
